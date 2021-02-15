2ab78375940b215851f0835ecbea00fa4bf98c15
PdoubleU <piotrwitasik1988@gmail.com>
2021-02-15 20:46:56 +0100

    1. add class move which is responsible for checking for current click status (first click when choosing piece or second click, when moving choosen piece on the particular square)

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
 class Board {
     constructor(){
-        // below define rows and columns, as well as first column name represents as integer 97 which is reffering to lower case letter a in asci
+        // below define rows and columns, as well as first column name represents as integer 97 which is refering to lower case letter a in asci
         this.rows = 8;
         this.firstColumn = 97;
         // property board represents the newly created board thanks to method createBoard()
         this.board = this.createBoard();
 
+        //call set of methods to populate already created board with uppercase or lowercase letters all refering particular piece. Uppercase for White pieces.
+        //P,p: pawns; R,r: rooks; N,n: knights; B,b: bishops; Q,q: queen; K,k: king:
         this.populatePawns();
         this.populateRooks();
         this.populateKnights();
@@ -55,10 +57,82 @@ class Board {
         this.board[String.fromCharCode(101)][0][1] = 'K';
         this.board[String.fromCharCode(101)][7][1] = 'k';
     }
+    // method called just after creating the board. This method returns html tags with elements referencing to all squares on chessboard with their proper id's:
+    printBoard(){
+        let color = 'b';
+        let container = document.createElement('div');
+        container.classList.add('board_container')
+        for (let elem in this.board) {
+            let column = document.createElement('div');
+            column.classList.add('column', `${elem}`);
+            container.append(column);
+            for (let i = 0; i < this.board[elem].length; i++) {
+                let square = document.createElement('div');
+                square.classList.add('square', `${this.board[elem][i][1]}`, `${color}`);
+                square.id = `${elem}${this.board[elem][i][0]}`;
+                square.textContent = `${elem}${this.board[elem][i][0]}`;
+                container.lastChild.prepend(square);
+                if (i === this.board[elem].length - 1)  {
+                    color = color;
+                } else {
+                    color = color === 'b' ? 'w' : 'b';
+                }
+            }
+        }
+        return container;
+    }
 }
 
+class Move {
+    constructor(id, piece, isFirst) {
+        this.id = id;
+        this.piece = piece;
+        this.isFirst = isFirst;
+        this.currentSquare = null;
+        this.nextSquare = null;
+    }
+
+    firstClick() {
+        if (this.isFirst !== null) {
+            console.log('false, this is not first click!');
+            return false;
+        } else {
+            this.currentSquare = this.id;
+            console.log('first click!');
+            return true;
+        }
+    }
+    secondClick() {
+        if (this.isFirst === null) {
+            console.log('false, this is not second click!');
+            return false;
+        } else {
+            this.nextSquare = this.id;
+            console.log('second click!');
+            return true;
+        }
+    }
+}
 
 
 let newGame = new Board();
 console.log(newGame.board);
+
+let root = document.querySelector('#root');
+root.append(newGame.printBoard())
+
+let isFirst = null;
+
+document.querySelectorAll('.square').forEach(element => element.addEventListener('click', e => {
+            let checkMove = new Move(e.target.id, null, isFirst)
+            if(checkMove.firstClick()) {
+                isFirst = e.target.id;
+            }
+            if (checkMove.secondClick()) {
+                isFirst = null;
+            };
+        }
+    )
+)
+
 
