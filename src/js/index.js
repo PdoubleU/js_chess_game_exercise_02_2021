259d0431f5e6f691b8bbd330a49181081b043f4f
PdoubleU <piotrwitasik1988@gmail.com>
2021-03-10 20:08:20 +0100

    1. set move rules for class Knight (capturing and basic move) 2. set move rules for class Bishop (capturing and basic move)

--- src/js/index.js
+++ src/js/index.js
@@ -477,6 +477,7 @@ class Rook {
             objOnTargSquare = this._board.board[this._targetSquare.split('')[0]][parseInt(this._targetSquare.split('')[1] - 1)][1],
             capture = this.isMoveCapture([targetY, currentY, targetX, currentX, objOnTargSquare]),
             basic = this.isMoveStraight([targetY, currentY, targetX, currentX]),
+            // to do (castling)
             special = this.isMoveSpecial([targetY, currentY, targetX, currentX]),
             obstacle = this.isMoveObstacle([targetY, currentY, targetX, currentX]);
 
@@ -500,20 +501,13 @@ class Rook {
         return  { x : 0, y : 0 }
     }
     specialMoveObj() {
+        // to do (castling)
         let obj;
         this.color === 'white'
         ?
         obj = { x : 0, y : 2 }
         :
         obj = { x : 0, y : -2 }
-    }
-    captureMoveObj() {
-        let obj;
-        this.color === 'white'
-        ?
-        obj = { x : [-1, 1], y : 1 }
-        :
-        obj = { x : [-1, 1], y : -1 }
     }
     isMoveStraight(args){
         if ((args[0] - args[1] == this.basicMoveObj.y) || (args[2] - args[3] == this.basicMoveObj.x)) {
@@ -583,10 +577,78 @@ class Knight {
         this.id = id;
         this.color = color;
         this.position = coordinates;
-        this.move = null;
-        this.specialMove = true;
-        this.promote = true;
-        this.capture = null;
+        this._targetSquare = null;
+        this._board = null;
+        this._isMoveValid = null;
+
+        this.basicMoveObj = this.straightMoveObj();
+    }
+    set validateMove(args) {
+        this._targetSquare = args[1]
+        this._board = args[2]
+        let targetX = this._targetSquare.split('')[0].charCodeAt(0),
+            targetY = parseInt(this._targetSquare.split('')[1]),
+            currentX = this.position[0].charCodeAt(0),
+            currentY = parseInt(this.position[1]),
+            objOnTargSquare = this._board.board[this._targetSquare.split('')[0]][parseInt(this._targetSquare.split('')[1] - 1)][1],
+            capture = this.isMoveCapture([targetY, currentY, targetX, currentX, objOnTargSquare]),
+            basic = this.isMoveStraight([targetY, currentY, targetX, currentX]);
+
+        if (capture && basic) {
+            return this._isMoveValid = true;
+        }
+        else {
+            return this._isMoveValid = false;
+        }
+
+    }
+    get validateMove() {
+        return this._isMoveValid
+    }
+    //update set of coordinates:
+    set updatePosition(coors) {
+        return this.position = [coors.split('')[0], parseInt(coors.split('')[1])]
+    }
+    straightMoveObj() {
+        return  {
+                    setOne: {
+                            x: 1,
+                            y: 2
+                    },
+                    setTwo: {
+                            x: 2,
+                            y: 1
+                    }
+                }
+    }
+    isMoveStraight(args){
+        if ((Math.abs(args[0] - args[1]) == this.basicMoveObj.setTwo.x) &&
+            (Math.abs(args[2] - args[3]) == this.basicMoveObj.setTwo.y) ||
+            (Math.abs(args[0] - args[1]) == this.basicMoveObj.setOne.x) &&
+            (Math.abs(args[2] - args[3]) == this.basicMoveObj.setOne.y)) {
+                return true
+            }
+            else {
+                return false
+            }
+    }
+    isMoveCapture(args){
+        if (this.isCapturedPieceEnemy(args[4])) {
+            return true
+        } else {
+            return false
+        }
+    }
+    isCapturedPieceEnemy(targPiece) {
+        if (targPiece === null) {
+            return true
+        }
+        else if (this.color === 'white')  {
+            return (/[a-z]_/.test(targPiece))
+        }
+        else {
+            return (/[A-Z]_/.test(targPiece))
+        }
     }
 }
 class Bishop {
